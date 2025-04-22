d2e3916dcb4a9b67bd3f5134384eaad8b523f005
Ed Ropple <ed@edropple.com>
2025-04-21 22:46:43 -0400

    started noodling on auth; need to intelligently merge the identivine auth concepts with our users.

--- apps/site/src/lib/server/db/schema/index.ts
+++ apps/site/src/lib/server/db/schema/index.ts
@@ -1,4 +1,5 @@
-import { pgTable, uuid, timestamp, text, boolean, jsonb, pgEnum } from "drizzle-orm/pg-core";
+import cryptoRandomString from "crypto-random-string";
+import { pgTable, uuid, timestamp, text, boolean, jsonb, pgEnum, integer } from "drizzle-orm/pg-core";
 import { ulid, ulidToUUID } from "ulidx";
 
 import { type StringUUID } from "../../../ext/typebox/index.js";
@@ -24,3 +25,61 @@ export const S3_LOCATOR_MIXIN = {
   bucket: text("bucket").notNull(),
   objectName: text("object_name").notNull(),
 };
+
+
+// ---------- DOMAIN OBJECTS ---------------------- //
+
+export const USERS = pgTable("users", {
+  userUuid: ULIDAsUUID().primaryKey(),
+
+  displayName: text().notNull().unique(),
+
+  email: text().notNull().unique(),
+  emailVerifiedAt: timestamp({
+    withTimezone: true,
+    mode: "date",
+  }),
+
+  active: boolean().notNull().default(true),
+
+  /**
+   * Increment this value to invalidate all existing tokens.
+   */
+  tokenSalt: integer().notNull().default(1),
+
+  ...TIMESTAMPS_MIXIN,
+});
+
+export const USER_AVATARS = pgTable("user_avatars", {
+  userAvatarUuid: ULIDAsUUID().primaryKey(),
+
+  userUuid: uuid()
+    .references(() => USERS.userUuid)
+    .notNull()
+    .unique(),
+
+  ...S3_LOCATOR_MIXIN,
+  ...TIMESTAMPS_MIXIN,
+});
+
+export const USER_EMAIL_VERIFICATION_TOKENS = pgTable(
+  "user_email_verification_tokens",
+  {
+    userUuid: uuid()
+      .references(() => USERS.userUuid)
+      .notNull(),
+    token: text("token")
+      .notNull()
+      .unique()
+      .$default(() =>
+        cryptoRandomString({ length: 32, type: "distinguishable" }),
+      ),
+
+    expiresAt: timestamp({
+      withTimezone: true,
+      mode: "date",
+    }).notNull(),
+
+    ...TIMESTAMPS_MIXIN,
+  },
+);
